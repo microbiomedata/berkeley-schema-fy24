4d18cfb8a8e22a97f4962a6604910c68c81acbe2
brynnz22 <brynn.zalmanek@pnnl.gov>
2023-12-28 14:02:01 -0800

    add doctest

--- nmdc_schema/migrators/Migrator_from_X_to_pr2_and_pr24.py
+++ nmdc_schema/migrators/Migrator_from_X_to_pr2_and_pr24.py
@@ -14,6 +14,18 @@ class Migrator_from_X_to_pr2_and_pr24(MigratorBase):
         ]
 
     def change_class_names(self, database: dict):
+        r"""
+        Transforms class/collection names into a new desired name"
+        
+        >>> m = Migrator_from_X_to_pr2_and_pr24()
+        >>> m.change_class_names({'mags_activity_set': {'id': 123, 'key': 'value'}, 'metabolomics_analysis_activity_set': {'id': 234,
+        ...     'key': 'value'}, 'metagenome_annotation_activity_set': {'id': 345, 'key': 'value'}, 'metagenome_sequencing_activity_set': {
+        ...     'id': 456, 'key': 'value'}, 'metaproteomics_analysis_activity_set': {'id': 567, 'key': 'value'}, 
+        ...     'metatranscriptome_activity_set': {'id': 678, 'key': 'value'}, 'nom_analysis_activity_set': {'id': 891, 'key': 'value'},
+        ...     'read_based_taxonomy_analysis_activity_set': {'id': 457, 'key': 'value'}, 'read_qc_analysis_activity_set': {'id': 764, 
+        ...     'key': 'value'}, 'omics_processing_set': {'id': 935, 'key': 'value'}})
+        {'mags_set': {'id': 123, 'key': 'value'}, 'metabolomics_analysis_set': {'id': 234, 'key': 'value'}, 'metagenome_annotation_set': {'id': 345, 'key': 'value'}, 'metagenome_sequencing_set': {'id': 456, 'key': 'value'}, 'metaproteomics_analysis_set': {'id': 567, 'key': 'value'}, 'metatranscriptome_analysis_set': {'id': 678, 'key': 'value'}, 'nom_analysis_set': {'id': 891, 'key': 'value'}, 'read_based_taxonomy_analysis_set': {'id': 457, 'key': 'value'}, 'read_qc_analysis_set': {'id': 764, 'key': 'value'}, 'data_generation_set': {'id': 935, 'key': 'value'}}
+        """
 
         collection_names_to_change = {
             "mags_activity_set": "mags_set",
