fb2c51a98082f7112ad090dc79f16b24d05753ac
eecavanna <eecavanna@users.noreply.github.com>
2024-06-06 17:02:33 -0700

    Fix typo in field name

--- nmdc_schema/migrators/migrator_from_X_to_PR193.py
+++ nmdc_schema/migrators/migrator_from_X_to_PR193.py
@@ -8,7 +8,7 @@ class Migrator(MigratorBase):
     This migrator adds a field named `replaces` to each document in the `workflow_chain_set` collection
     that has a predecessor. The value of the field is the `id` of that predecessor.
 
-    This migrator also adds a field named `ends_at_time` to each document in the `workflow_chain_set` collection.
+    This migrator also adds a field named `ended_at_time` to each document in the `workflow_chain_set` collection.
     The value of the field is an ISO 8601-formatted timestamp indicating when the `WorkflowChain`'s most recent
     `WorkflowExecution` ended.
 
@@ -26,9 +26,9 @@ class Migrator(MigratorBase):
     # Example: {'wfc-id-april': 'wfc-id-january', 'wfc-id-march': 'wfc-id-february', ...}
     id_to_replaces_map = dict()
 
-    # A mapping from `workflow_chain_set` document `id` to its eventual `ends_at_time` value, if any.
+    # A mapping from `workflow_chain_set` document `id` to its eventual `ended_at_time` value, if any.
     # Example: {'wfc-id-april': '2024-06-07T06:17:45+07:00', ...}
-    id_to_ends_at_time_map = dict()
+    id_to_ended_at_time_map = dict()
 
     def upgrade(self) -> None:
         r"""
@@ -44,7 +44,7 @@ class Migrator(MigratorBase):
         # Write the values to the documents.
         self.adapter.process_each_document(
             collection_name="workflow_chain_set",
-            pipeline=[self.set_replaces_field, self.set_ends_at_time_field],
+            pipeline=[self.set_replaces_field, self.set_ended_at_time_field],
         )
 
     def compile_replacements_and_end_times(self, workflow_chain: dict) -> None:
@@ -53,5 +53,5 @@ class Migrator(MigratorBase):
     def set_replaces_field(self, workflow_chain: dict) -> dict:
         raise NotImplementedError  # TODO
 
-    def set_ends_at_time_field(self, workflow_chain: dict) -> dict:
+    def set_ended_at_time_field(self, workflow_chain: dict) -> dict:
         raise NotImplementedError  # TODO
