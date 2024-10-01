393cb242578ae22e83746445b1f0e47338af6dfb
Mark A. Miller <MAM@lbl.gov>
2024-10-01 16:19:02 -0400

    all inlined_as_list as needed now

--- tests/test_all_multivalued_class_range_slots_are_inlined_as_list.py
+++ tests/test_all_multivalued_class_range_slots_are_inlined_as_list.py
@@ -9,13 +9,9 @@ from linkml_runtime.utils.schemaview import OrderedBy
 from linkml_runtime.linkml_model.meta import ClassDefinition
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
-SCHEMA_DIR = os.path.join(ROOT, "src", "schema")
+SCHEMA_DIR = os.path.join(ROOT, "nmdc_schema")
 
-SCHEMA_FILE = os.path.join(SCHEMA_DIR, 'nmdc.yaml')
-
-allowed_typeless_classes = set()
-
-allowed_typeless_classes.add('Database')
+SCHEMA_FILE = os.path.join(SCHEMA_DIR, 'nmdc_materialized_patterns.yaml')
 
 
 class TestAllMultivaluedClassRangeSlotsAreInlinedAsList(unittest.TestCase):
@@ -34,20 +30,17 @@ class TestAllMultivaluedClassRangeSlotsAreInlinedAsList(unittest.TestCase):
             current_induced_slots = current_induced_class.attributes
 
             for sk, sv in sorted(current_induced_slots.items()):
-                # print(f"{current_class} {sk}")
-                # print(f"{sv.range = }")
-                # print(f"{sv.multivalued = }")
-                #
-                # x = view.get_element(sv.range)
-                # print(type(x))
 
                 if isinstance(view.get_element(sv.range), ClassDefinition) and sv.multivalued:
+
                     # check if the class in the range has an identifiers slot
                     range_identifier = view.get_identifier_slot(sv.range)
-                    # print(f"{range_identifier = }")
+
                     if range_identifier is not None:
-                        # print(f"{current_class} {sk} {sv.range} {range_identifier}")
                         pass
                     else:
-                        # self.assertTrue(sv.inlined_as_list, msg=f"{current_class} {sk} is not inlined_as_list")
-                        print(f"{current_class}.{sk}")  # is not inlined_as_list
+                        print(
+                            f"{current_class}.{sk} is multivalued and has range {sv.range}, which does not have an identifying slot, so must be inlined_as_list")
+                        print(yaml_dumper.dumps(sv))
+                        self.assertTrue(sv.inlined_as_list,
+                                        f"{current_class}.{sk} is multivalued and has range {sv.range}, which does not have an identifying slot, so must be inlined_as_list")
